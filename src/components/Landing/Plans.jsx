09c82bc82a15bb2fd55919ae83bc7d84c1d24ea8
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-12-03 06:22:45 +0100

    Planning on trying vercel

--- src/components/Landing/Plans.jsx
+++ src/components/Landing/Plans.jsx
@@ -8,14 +8,14 @@ const Plans = () => {
             <div className="text-gray-400 text-lg text-center font-poppins">Below are the various investment plans that we provide for all our clients.</div>
 
             <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-2 content-center gap-3 pt-5 pb-5">
-                <PlanItem name={"Basic Package"} percentage={"20%"} minimum={"50"} maximum={"299"}
+                <PlanItem name={"Basic Package"} percentage={"20%"} minimum={"50"} maximum={"499"}
                     duration={"24"} refferal={"5%"} />
 
-                <PlanItem name={"Regular Package"} percentage={"50%"} minimum={"300"} maximum={"599"} duration={"48"} refferal={"5%"} />
+                <PlanItem name={"Regular Package"} percentage={"50%"} minimum={"500"} maximum={"999"} duration={"48"} refferal={"5%"} />
 
-                <PlanItem name={"Premium Package"} percentage={"80%"} minimum={"600"} maximum={"1,499"} duration={"72"} refferal={"10%"} />
+                <PlanItem name={"Premium Package"} percentage={"80%"} minimum={"1,000"} maximum={"9,999"} duration={"72"} refferal={"10%"} />
 
-                <PlanItem name={"VIP Package"} percentage={"100%"} minimum={"2,000"} maximum={"4,999"} duration={"96"} refferal={"10%"} />
+                <PlanItem name={"VIP Package"} percentage={"100%"} minimum={"10,000"} maximum={"20,000"} duration={"96"} refferal={"10%"} />
             </div>
         </div>
     )
